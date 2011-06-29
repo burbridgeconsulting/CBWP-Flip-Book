6beba7a693586fbc8e44692637b9ff6ebb01a5e0
Chris Burbridge <chris@chrisburbridge.com>
2011-06-29 13:20:10 -0700

    Have fading in / out for turning pages working!

--- js/magazine.js
+++ js/magazine.js
@@ -1,7 +1,13 @@
 jQuery(document).ready(function($) {
+                             
+  // Clicking right page moves us DOWN the pages  
+  $('.page.right').not('.last').click(function() {  
+      $(this).parent().fadeOut()
+  })
     
-  // $('.page').click(function() {  
-  //     $(this).fadeOut()
-  // })
+  // Clicking left page moves us UP the pages  
+  $('.page.left').not('.first').click(function() {  
+      $(this).parent().next().fadeIn()
+  })
     
 })
