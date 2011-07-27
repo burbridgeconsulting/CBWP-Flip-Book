51c54efb011dbd7d8abb6af4171100d7a544a73f
Chris Burbridge <chris@chrisburbridge.com>
2011-07-27 11:28:35 -0700

    Fixed problem with events propogating

--- js/magazine.js
+++ js/magazine.js
@@ -1,23 +1,24 @@
 jQuery(document).ready(function($) {                                
     
-  // Clicking right page moves us DOWN the pages  
-  $('.page.right').not('.last').click(function() {  
-      $(this).parent().fadeOut()
-  })
+    // Popup display
+    $('.popup').click(function(event) {
+        event.stopImmediatePropagation()
+        $(this).parent().find('.content').toggle() 
+    })
+
+    // Clicking right page moves us DOWN the pages  
+    $('.page.right').not('.last').click(function(event) {   
+        $(this).parent().fadeOut()
+    })
     
-  // Clicking left page moves us UP the pages  
-  $('.page.left').not('.first').click(function() {  
-      $(this).parent().next().fadeIn()
-  })        
+    // Clicking left page moves us UP the pages  
+    $('.page.left').not('.first').click(function(event) {  
+        $(this).parent().next().fadeIn()
+    })        
   
-  // Popup display
-  $('.popup').click(function() {
-      $(this).parent().find('.content').toggle()
-  })
-  
-  // Clicking anything else hides popup 
-  // $('body').not('.popup .msg').click(function() {
-  //     $('.popup .content').hide()
-  // })
+    // Clicking anything else hides popup 
+    // $('body').not('.popup .msg').click(function() {
+    //     $('.popup .content').hide()
+    // })
     
 })
